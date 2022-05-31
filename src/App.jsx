09a6cc37a64f5ea2139e5339ae95a81e3475a162
fadarash <fadarash@gmail.com>
2022-05-31 20:32:03 +0430

    setInterval

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App({ initialSeconds }) {
+  const [seconds, setSeconds] = useState(initialSeconds);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (seconds === 0) {
+        clearInterval(interval);
+        return
+      }
+      setSeconds((oldSeconds) => oldSeconds - 1);
+    }, 1000);
+    return () => clearInterval(interval)
+  }, [seconds]);
+
+  return (
+    <div className="App">
+      <h1 id="timer">{seconds}</h1>
+    </div>
+  );
+}
+
+export default App;
