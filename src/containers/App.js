a86e52704d2ff891ae855ceedc27a797361d6a37
fadarash <fadarash@gmail.com>
2022-06-28 15:48:06 +0430

    quera method

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,21 @@
-import React, { useState } from 'react'
-import MessageForm from '../components/MessageForm/MessageForm'
-import MessageList from '../components/MessageList/MessageList'
-import './App.css'
+import React, { useState } from "react";
+import MessageForm from "../components/MessageForm/MessageForm";
+import MessageList from "../components/MessageList/MessageList";
+import "./App.css";
 
 function App() {
-  const [messages, setMessages] = useState([])
-
-
+  const [messages, setMessages] = useState([]);
 
   const onMessageSend = (message, me) => {
-    setMessages((old) => ([...old, { me: me, body: message }]))
-  }
+    setMessages((old) => [...old, { me: me, body: message }]);
+  };
 
   return (
     <div className="App">
       <MessageList messages={messages} />
       <MessageForm onMessageSend={onMessageSend} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
